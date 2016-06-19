45f4e72c9645868724973801619f0b437b4a9bc8
Erasmo Leite Junior <eleitejr@gmail.com>
2016-06-19 12:45:04 -0300

    Update AboutDialog.java

--- src/gui/CustomAboutDialog.java
+++ src/gui/CustomAboutDialog.java
@@ -23,62 +23,64 @@ import de.javasoft.swing.AboutDialog;
 @SuppressWarnings("serial")
 public class CustomAboutDialog extends JFrame
 {  
-  /**
-   * Constructor for the main application.  
-   */
-  public CustomAboutDialog()
-  {
-    super("Custom AboutDialog Demo");    
-    setLayout(new FlowLayout());
-    
-    JButton button = new JButton("Open Custom AboutDialog");
-    button.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent evt)
-      {
-        try
-        {
-          AboutDialog ad = new AboutDialog(SwingUtilities.getWindowAncestor((Component)evt.getSource()), false, null, false);
-          ad.setTitle("My Custom About Dialog");
-          ad.setBackgroundImage("/de/javasoft/synthetica/democenter/examples/aboutdialog/aboutDialogBackground.png", new Insets(0, 0, 1, 0));
-          ad.setDescription("<html><b>About CustomAboutDialog</b><p>Click on the tabs below to get informations about the software and the system.</html>");
-          ad.setAboutText(this.getClass().getResource("about.html"));
-          ad.showDialog();
-        }
-        catch (IOException e)
-        {
-          e.printStackTrace();
-        }
-      }
-    });
-    add(button);
-    
-    //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    setSize(320, 240);
-    setLocationRelativeTo(null);        //center
-    setVisible(true);
-  }
+	/**
+	 * Constructor for the main application.  
+	 */
+	public CustomAboutDialog()
+	{
+		super("Viajantes Espaciais");    
+		setLayout(new FlowLayout());
 
-  /**
-   * Static main method for application startup. 
-   */
-  public static void main(String[] args)
-  {
-    EventQueue.invokeLater(new Runnable()
-    {
-      public void run()
-      {
-        try
-        {
-          UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaStandardLookAndFeel");
-          new CustomAboutDialog();
-        }
-        catch (Exception e)
-        {
-          e.printStackTrace();
-        }
-      }
-    });
-  }
+		JButton button = new JButton("Abrir Diálogo SOBRE");
+		button.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent evt)
+			{
+				try
+				{
+					AboutDialog ad = new AboutDialog(SwingUtilities.getWindowAncestor((Component)evt.getSource()), false, null, false);
+					ad.setTitle("Astronaut Database");
+					ad.setBackgroundImage("Astronaut-50.png", new Insets(0, 0, 1, 0));
+					ad.setDescription("<html><b>Sobre Astronaut Database</b><p>Clique nas abas abaixo para informações sobre o software e o sistema.</html>");
+					ad.setAboutText(this.getClass().getResource("about.html"));
+					ad.showDialog();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+		add(button);
+
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(320, 240);
+		setLocationRelativeTo(null);        //center
+		setVisible(true);
+	}
+
+	/**
+	 * Static main method for application startup. 
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaPlainLookAndFeel");
+					new CustomAboutDialog();
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
